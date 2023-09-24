fa6d894a907adadfde6abca394041808f110ec78
Jkomba <johnkombah88@gmail.com>
2023-09-24 18:09:09 +0300

    fixed errors resulting in code not working

--- grading/grading.js
+++ grading/grading.js
@@ -1,15 +1,24 @@
 function getGrade(mark) {
-    if (mark > 79) {
-      return "A";
-    } else if (mark >= 60 && mark <= 79) {
-      return "B";
-    } else if (mark >= 50 && mark <= 59) {
-      return "C";
-    } else if (mark >= 40 && mark <= 49) {
-      return "D";
-    } else {
-      return "E";
-    }
+  if (mark > 79) {
+    return "A";
+  } else if (mark >= 60 && mark <= 79) {
+    return "B";
+  } else if (mark >= 50 && mark <= 59) {
+    return "C";
+  } else if (mark >= 40 && mark <= 49) {
+    return "D";
+  } else {
+    return "E";
   }
-  
-  
+}
+
+function getUserInput() {
+  const mark = parseInt(prompt("Enter the student's mark (between 0 and 100):"));
+
+  if (!isNaN(mark) && mark >= 0 && mark <= 100) {
+    const grade = getGrade(mark);
+    alert(`The grade for ${mark} is ${grade}`);
+  } else {
+    alert("Invalid input. Please enter a number between 0 and 100.");
+  }
+}
